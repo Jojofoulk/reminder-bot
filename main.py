dd8c078d36143efbe4f51d73e9937755d5266671
Josselin Francois Downey <jossfrancois@gmail.com>
2020-09-23 18:16:42 +1000

    change presence

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ bot = commands.Bot(command_prefix="!")
 @bot.event
 async def on_ready():
     print("Bot running")
-    activity = discord.Activity(name=f'Reminding you of stuff', type=discord.ActivityType.watching)
+    activity = discord.Activity(name=f'Reminding you of stuff', type=discord.ActivityType.playing)
     await bot.change_presence(status = discord.Status.online, activity=activity)
 
 @bot.event
